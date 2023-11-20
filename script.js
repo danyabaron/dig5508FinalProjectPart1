f3b6abbb438ce9fa6326718a9e1ad187c39ab06b
lucidbard <lucidbard@users.github.com>
2023-11-20 17:21:06 +0000

    Nov 20, 2023, 12:21 PM

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function setup() {
 
     angleMode(DEGREES);
     createCanvas(600, 600);
-    randomSeed(22);
+    randomSeed(220);
 
     orbits.push(new Orbit(width / 2, height / 2, 160));
     orbits.push(new Orbit(width / 2, height / 2, 230));
