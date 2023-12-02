c53d44ca2c4d051b699a26657ea80ac281f305ef
Danya Baron <dbaron@wpi.edu>
2023-12-02 22:36:03 +0000

    Dec 2, 2023, 5:36 PM

--- script.js
+++ script.js
@@ -272,8 +272,12 @@ function keyPressed() {
                     alignment = CONJUNCTION;                                      
                     console.log('conjunction');
                     console.log("pause boolean in conjunction " + isPaused);
+                    if (isPaused) {
                     planets[i].drawLine = false; // Set drawLine property to false to clear line
                     console.log('cleared line');
+
+                    }
+                    
                 } else if (59 <= mvAngleDifference && mvAngleDifference <= 65) {
                     planets[i].drawLine = true; // Set drawLine property to true for sextile
                     alignment = SEXTILE;   
