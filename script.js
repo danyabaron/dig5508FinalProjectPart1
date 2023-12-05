96de64cefd3f8d802aeb53cd8dc0d8efd3e05db7
Danya Baron <dbaron@wpi.edu>
2023-12-05 18:38:59 +0000

    Dec 5, 2023, 1:38 PM

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function setup() {
     planets.push(new Planet(20, 120, 0.8, 30, color('#626f96'), "Mercury"));
     planets.push(new Planet(30, 175, 0.4, 100, color('#de6840'), "Venus"));
     // earth = new Planet(20, 230, 0.6, 190, color('green'));
-    planets.push(new Planet(30, 240, 1, 220, color('red'), "Mars"));
+    // planets.push(new Planet(30, 240, 1, 220, color('red'), "Mars"));
     // planets.push(new Planet(50, 340, 0.3, 60, color('#7e79a3'), "Jupiter"));
     // planets.push(new Planet(50, 450, 0.1, 120, color('gray'), "Saturn"));
    
